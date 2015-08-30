d73aa9344709c293125e15653e47b2664c346b21
Vince Mi <vinc3m1@gmail.com>
2015-08-30 16:02:28 -0700

    sample inject into Activity

--- sample/src/main/kotlin/com/makeramen/stiletto/sample/StilettoApp.kt
+++ sample/src/main/kotlin/com/makeramen/stiletto/sample/StilettoApp.kt
@@ -1,10 +1,14 @@
 package com.makeramen.stiletto.sample
 
 import android.app.Application
+import kotlin.properties.Delegates
 
 public class StilettoApp : Application() {
 
+  val component = ApplicationComponent(this)
+
   override fun onCreate() {
     super.onCreate()
+
   }
 }
--- sample/src/main/kotlin/com/makeramen/stiletto/sample/ui/MainActivity.kt
+++ sample/src/main/kotlin/com/makeramen/stiletto/sample/ui/MainActivity.kt
@@ -2,8 +2,13 @@ package com.makeramen.stiletto.sample.ui
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.makeramen.stiletto.sample.StilettoApp
+import kotlin.properties.Delegates
 
 public class MainActivity : AppCompatActivity() {
+
+  val component by Delegates.lazy { (getApplicationContext() as StilettoApp).component }
+  val sharedPrefs by Delegates.lazy { component.sharedPreferences() }
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
